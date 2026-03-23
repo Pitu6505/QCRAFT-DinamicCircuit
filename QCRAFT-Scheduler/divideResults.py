3ce5f75971299301bfd63bf413948326dc30641d
Pitu6505 <asanchezmu@aumnos.unex.es>
2026-03-23 10:05:53 +0100

    Intento de arreglode commit

--- QCRAFT-Scheduler/divideResults.py
+++ QCRAFT-Scheduler/divideResults.py
@@ -55,19 +55,27 @@ def divideResults(counts:dict, shots:list, provider:str, qb:list, users:list, ci
         list: The results of the circuit execution divided among the users.
     """
     result = []
+    total_qb = sum(qb)
     for i in range(len(shots)):
         
         newCounts = {}
 
         for key, value in counts.items(): #Reducing each dictionary so that it contains the useful part of each user
+            # Algunos backends/samplers devuelven solo los bits medidos (ej. 3 bits)
+            # en vez del ancho total del circuito compuesto (ej. 5 bits).
+            # Normalizamos para que el slicing por usuario sea consistente.
+            key_norm = key
+            if isinstance(key_norm, str) and len(key_norm) < total_qb:
+                key_norm = key_norm.rjust(total_qb, '0')
+
             rightRemovedQubits = sum(qb[0:i])  #Values to remove from the right
             leftRemovedQubits = sum(qb[i+1:len(qb)])  #Values to remove from the left
             if provider == 'aws':
-                data = key[rightRemovedQubits:]  #Data is the custom value of each user
+                data = key_norm[rightRemovedQubits:]  #Data is the custom value of each user
                 data = data[:(len(data)-leftRemovedQubits)]
                 data = data[::-1] #AWS gives the results backwards compared to IBM, to have a standard, the result is reversed
             else:
-                data = key[leftRemovedQubits:]  #Data is the custom value of each user
+                data = key_norm[leftRemovedQubits:]  #Data is the custom value of each user
                 data = data[:(len(data)-rightRemovedQubits)]
             
             if data in newCounts:
